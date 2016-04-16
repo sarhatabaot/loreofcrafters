f3b6ecc073ac5704682cabc2037a2f72560cf607
sarhatabaot <sarhatabaot1@gmail.com>
2016-04-16 11:17:08 +0300

    Revert "Changed to using "data-hook"s instead of classes"
    This reverts commit 3d32eabe3cb0744815fe179a4a1afcd3fb021460.

--- profile.js
+++ profile.js
@@ -1,16 +1,3 @@
-$.extend({
-    hook: function(hookName) {
-        var selector;
-        if(!hookName || hookName === '*') {
-            // select all data-hooks
-            selector = '[data-hook]';
-        } else {
-            // select specific data-hook
-            selector = '[data-hook~="' + hookName + '"]';
-        }
-        return $(selector);
-    }
-});
 $(document).ready(function() {
     setTimeout(
         function() 
@@ -25,43 +12,43 @@ $(document).ready(function() {
             //Username, guest, join, register and basic menu items
             if (username === ''){ 
                 //What Guest sees
-                $.hook('nav-username nav-link').prepend("Guest <b class=\"caret\"></b>");
-                $.hook('nav-username nav-dropdown-menu').append("<li class=\"dropdown-item\"><a href=\"/login\" >Login</a></li>");
-                $.hook('nav-username nav-dropdown-menu').append("<li class=\"dropdown-item\" data-hook=\"nav-register\"></li>");
-                $.('#enjin-bar a[href$="/register"]').prependTo($.hook('nav-register'));
+                $("#boot_username a").prepend("Guest <b class=\"caret\"></b>");
+                $("#boot_username >.dropdown-menu").append("<li class=\"dropdown-item\"><a href=\"/login\" >Login</a></li>");
+                $("#boot_username >.dropdown-menu").append("<li class=\"dropdown-item\" id=\"boot_register\"></li>");
+                $('#enjin-bar a[href$="/register"]').prependTo($('#boot_register'));
             }
             else{
-                $.hook("nav-username nav-link").prepend(username+"<b class=\"caret\"></b>");
-                $.hook("nav-username nav-dropdown-menu").append("<li class=\"dropdown-item\" data-hook=\"nav-join\"></li>");
-                $.hook('#enjin-bar .right .join-site a').prependTo($.hook('nav-join'));
-                $.hook("nav-username nav-dropdown-menu").append("<li class=\"dropdown-item\"><a href=\"/profile\" target=\"_blank\">Profile</a></li>");
-                $.hook("nav-username nav-dropdown-menu").append("<li class=\"dropdown-item\"><a href=\"/dashboard/settings\" target=\"_blank\">Account Settings</a></li>");
-                $.hook("nav-username nav-dropdown-menu").append("<li class=\"dropdown-item\"><a href=\"/dashboard/settings/website\" target=\"_blank\">Website Settings</a></li>");
+                $("#boot_username >a").prepend(username+"<b class=\"caret\"></b>");
+                $("#boot_username >.dropdown-menu").append("<li class=\"dropdown-item\" id=\"boot_join\"></li>");
+                $('#enjin-bar .right .join-site a').prependTo($('#boot_join'));
+                $("#boot_username >.dropdown-menu").append("<li class=\"dropdown-item\"><a href=\"/profile\" target=\"_blank\">Profile</a></li>");
+                $("#boot_username >.dropdown-menu").append("<li class=\"dropdown-item\"><a href=\"/dashboard/settings\" target=\"_blank\">Account Settings</a></li>");
+                $("#boot_username >.dropdown-menu").append("<li class=\"dropdown-item\"><a href=\"/dashboard/settings/website\" target=\"_blank\">Website Settings</a></li>");
                 // Load messages
-                $.hook("nav-username nav-dropdown-menu").append("<li class=\"dropdown-item\" data-hook=\"nav-messages\"></li>");
-                $.hook("nav-username nav-dropdown-menu").append("<li class=\"dropdown-item\" data-hook=\"nav-applications\"></li>");
-                $.hook("nav-username nav-dropdown-menu").append("<li class=\"dropdown-item\" data-hook=\"nav-dashboard\"></li>");
-                $.hook("nav-messages").append("<a href=\"/dashboard/messages\"><span class=\"badge label label-default label-pill pull-xs-right\">"+ messages +" </span> Messages</a>");
+                $("#boot_username >.dropdown-menu").append("<li class=\"dropdown-item\" id=\"boot_messages\"></li>");
+                $("#boot_username >.dropdown-menu").append("<li class=\"dropdown-item\" id=\"boot_applications\"></li>");
+                $("#boot_username >.dropdown-menu").append("<li class=\"dropdown-item\" id=\"boot_dashboard\"></li>");
+                $("#boot_messages").append("<a href=\"/dashboard/messages\"><span class=\"badge label label-default label-pill pull-xs-right\">"+ messages +" </span> Messages</a>");
                 //Applications display
-                $("nav-applications").append("<a href=\"/dashboard/applications\"><span class=\"badge label-default label-pill pull-xs-right\">"+ applications +" </span> Applications</a>");
+                $("#boot_applications").append("<a href=\"/dashboard/applications\"><span class=\"badge label-default label-pill pull-xs-right\">"+ applications +" </span> Applications</a>");
                 //Notification count on dashboard link display
-                $("nav-dashboard").append("<a href=\"/dashboard\" target=\"_blank\"><span class=\"badge label-default label-pill pull-xs-right\">"+ notifications +" </span> Dashboard</a>");
+                $("#boot_dashboard").append("<a href=\"/dashboard\" target=\"_blank\"><span class=\"badge label-default label-pill pull-xs-right\">"+ notifications +" </span> Dashboard</a>");
                 //Admin link display, admin only
                 if (adminstatus === ''){
-                    $("nav-username nav-dropdown-menu").append("<li class=\"dropdown-item\"><a href=\"/admin\">Admin</a></li>");
+                    $("#boot_username >.dropdown-menu").append("<li class=\"dropdown-item\"><a href=\"/admin\">Admin</a></li>");
                 }
                 // Load like button
                 if (username !== ''){ 
-                    $("nav-username nav-dropdown-menu").append("<li class=\"dropdown-divider\"></li>");
-                    $("nav-username nav-dropdown-menu").append("<li class=\"dropdown-header \">Enjin Popularity</li>");
-                    $("nav-username nav-dropdown-menu").append($("#enjin-like-site"));
-                    $("nav-username nav-dropdown-menu").append("<li class=\"dropdown-divider\"></li>");
-                    $("nav-username nav-dropdown-menu").append("<li class=\"dropdown-header\">Account</li>");
-                    $("nav-username nav-dropdown-menu").append("<li class=\"dropdown-item\"><a href=\"/logout\">Logout</a></li>");
+                    $("#boot_username >.dropdown-menu").append("<li class=\"dropdown-divider\"></li>");
+                    $("#boot_username >.dropdown-menu").append("<li class=\"dropdown-header \">Enjin Popularity</li>");
+                    $("#boot_username >.dropdown-menu").append($("#enjin-like-site"));
+                    $("#boot_username >.dropdown-menu").append("<li class=\"dropdown-divider\"></li>");
+                    $("#boot_username >.dropdown-menu").append("<li class=\"dropdown-header\">Account</li>");
+                    $("#boot_username >.dropdown-menu").append("<li class=\"dropdown-item\"><a href=\"/logout\">Logout</a></li>");
                 }
             }
            
             //Display username after scripts finish
-            $.hook("nav-username").show();
+            $( "#boot_username" ).show();
         }, 1);
 });
